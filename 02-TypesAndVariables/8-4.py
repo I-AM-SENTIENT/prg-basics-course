54cfba56bc71b22555e3f7e1847af2c222825a57
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-10-26 20:52:14 +0100

    I love useless units

---
+++ 02-TypesAndVariables/8-4.py
@@ -0,0 +1,7 @@
+# A program that prints your height both in cm and in feet and inches.
+
+cm = 170
+feet = cm // 30.48
+inches = (cm % 30.48) // 2.54
+
+print(f'I am {cm}cm tall, i.e. {feet} feet and {inches} inches')
