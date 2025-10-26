d269d89527ddc68a2305e2c39c4bd6c54cc83931
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-10-26 20:35:13 +0100

    Some tasks

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = 25
+height = 175.5
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"My height is {height} cm.")
+
