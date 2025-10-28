bb792e87d368153de996351baa954271b04c6ed7
I-AM-SENTIENT <am.i.sentient.i.am@gmail.com>
2025-10-28 22:50:59 +0100

    dunno

--- 04-Functions/test_converters.py
+++ 04-Functions/test_converters.py
@@ -2,5 +2,6 @@ import converters
 
 print('### Test converters')
 print(f'Three meters is {converters.m_to_cm(3)}cm')
+print(f'300cm is {converters.cm_to_m(300)}m')
 
 
---
+++ 05-MockTest/test-3.py
@@ -0,0 +1,7 @@
+n = 5
+result = 1 # calculate the factorial for this value
+
+for i in range(n):
+    result *= i+1
+
+print(f"Factorial of {n} is:", result) 
